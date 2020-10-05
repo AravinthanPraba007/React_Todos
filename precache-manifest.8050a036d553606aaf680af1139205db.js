53a46b01dcce890f2f5600b2e761646b2c87b761
Aravinthan Praba <799532>
2020-10-05 23:14:31 +0530

    Updates

---
+++ precache-manifest.8050a036d553606aaf680af1139205db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e481fb36de968e5593c5c34edff7bf8",
+    "url": "/React_Todos/index.html"
+  },
+  {
+    "revision": "64d93e5ebe40db1b84d7",
+    "url": "/React_Todos/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c86929a0a2a72e020240",
+    "url": "/React_Todos/static/js/2.9a90fa15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Todos/static/js/2.9a90fa15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d93e5ebe40db1b84d7",
+    "url": "/React_Todos/static/js/main.8ec51eb4.chunk.js"
+  },
+  {
+    "revision": "ff248facee167b41a235",
+    "url": "/React_Todos/static/js/runtime-main.ffe96560.js"
+  }
+]);
