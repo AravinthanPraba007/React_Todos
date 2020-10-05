a08a6b4231062c288058047e1adfd7e4822c3346
Aravinthan Praba <799532>
2020-10-05 22:43:10 +0530

    Implemented the creating new todos functionalaity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
-
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoInputRef = useRef()
+  const LOCAL_STORAGE_KEY ='todoApp.todos'
+
+  //  If todo is empty we are trying to getting back stored todos form local storage
+  useEffect(() => {
+    const storedTodo =JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+
+    if(storedTodo) setTodos(storedTodo)
+  },[])
+
+  // Storing the existing todo in local storage 
+  useEffect(() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos))
+  },[todos])
+
+ 
+
+  function handleAddTodo(e) {
+    const task = todoInputRef.current.value
+    if(task === '') return
+    setTodos( prevTodos => {
+      return [...prevTodos, {id:uuidv4(), name: task, completed: false}]
+    })
+    todoInputRef.current.value=null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <TodoList todos={todos} />
+      <input ref={todoInputRef} type="text" />
+      <button onClick= {handleAddTodo}>Add To do</button>
+      <button>Delete all</button>
+      <div>
+        0 Pending Task
     </div>
+    </>
+
   );
 }
 
